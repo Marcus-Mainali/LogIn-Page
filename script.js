b92ccf99262402459a82995f1d2a85015a2af3f3
Marcus Mainali <marcusmainali@gmail.com>
2024-10-19 12:01:26 +0545

    Minecraft Themed LogIn Page

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.getEmementById('form')
+const firstname_input = document.getElementById('firstname-input')
+const email_input = document.getElementById('email-input')
+const password_input = document.getElementById('password-input')
+const repeat_password_input = document.getElementById('repeat-password-input')
